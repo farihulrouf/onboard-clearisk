2136bbb89ebe495924668aea9af92fc4700a4bc1
farihulrouf <farihulrouf@gmail.com>
2022-05-09 17:32:31 +0700

    connect

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-    "github.com/gofiber/fiber/v2"
+    "BE_CLEARISK.IO/configs" 
+    "github.com/gofiber/fiber/v2" 
 )
 
 func main() {
     app := fiber.New()
-  
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.JSON(&fiber.Map{"data": "Hello from Fiber & mongoDB Siplah"})
-    })
+
+    //run database
+    configs.ConnectDB()
+
     app.Listen("127.0.0.1:2222")
-    ///app.Listen(":8111")
-    //app.Listen(8282)
-    //router.Run(":8111")
 }
 
 /*
