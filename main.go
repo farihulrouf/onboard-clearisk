4b71f3811abe957fd2b6d0edeb18b79707326fa8
farihulrouf <farihulrouf@gmail.com>
2022-05-09 17:22:13 +0700

    init 2.1

--- main.go
+++ main.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"BE_CLEARISK.IO/config"
-	"BE_CLEARISK.IO/database"
-	"net/http"
-	"github.com/gorilla/mux"
-	
+    "github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	conf := config.GetConfig()
-	//db := database.ConnectDB(ctx, conf.Mongo)
-	db := database.ConnectDB(conf.Mongo)
-	fmt.Println(db)
-	r := mux.NewRouter()
-	http.ListenAndServe(":8222",r)
-
-	/*
-	db := database.ConnectDB(ctx, conf.Mongo)
-	collection := db.Collection(conf.Mongo.Collection)
-	*/
-
-	
+    app := fiber.New()
+  
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.JSON(&fiber.Map{"data": "Hello from Fiber & mongoDB Siplah"})
+    })
+    app.Listen("127.0.0.1:2222")
+    ///app.Listen(":8111")
+    //app.Listen(8282)
+    //router.Run(":8111")
 }
+
+/*
+app.Listen(8080)
+app.Listen("8080")
+app.Listen(":8080")
+app.Listen("127.0.0.1:8080")
+*/
